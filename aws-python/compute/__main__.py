94065f589c1fba9d9a51df914df05a4eb9f6f699
Mirko Ruzic <mirko.ruzic@live.com>
2023-08-19 03:16:13 +0200

    ec2_instance.py component added. Public IP associating from the config completed. Fetching security group from the networking project added

--- aws-python/compute/__main__.py
+++ aws-python/compute/__main__.py
@@ -5,7 +5,7 @@ from pulumi import Config, StackReference
 def get_security_group_ids(security_group_names):
     security_group_ids = []
     for sg_name in security_group_names:
-        sg_id = networking_stack.output[f"{sg_name}_security_group_id"]
+        sg_id = networking_stack.get_output(f"{sg_name}_security_group_id")
         security_group_ids.append(sg_id)
     return security_group_ids
 
@@ -19,7 +19,6 @@ current_stack = pulumi.get_stack()
 # Reference the networking stack using the same stack name
 networking_stack = StackReference(f"mruzic/networking/{current_stack}")
 
-# Loop through the EC2 instances defined in the configuration
 for ec2_instance_config in ec2_instances:
     name = ec2_instance_config["name"]
     number = ec2_instance_config["number"]
@@ -27,32 +26,37 @@ for ec2_instance_config in ec2_instances:
     instance_type = ec2_instance_config["instanceType"]
     subnet_type = ec2_instance_config["subnetType"]
     security_group_names = ec2_instance_config["securityGroupName"]
+    associate_public_ip = ec2_instance_config.get("associatePublicIp", False) if subnet_type == "public" else False
 
     # Get security group IDs based on names
     security_group_ids_list = get_security_group_ids(security_group_names)
 
     # Based on the subnet type, decide which subnet ID to use
     if subnet_type == "public":
-        subnet_id = networking_stack.output.public_subnet_id
+        subnet_id = networking_stack.get_output("public_subnet_id")
     elif subnet_type == "private":
-        subnet_id = networking_stack.output.private_subnet_id
+        subnet_id = networking_stack.get_output("private_subnet_id")
     else:
         raise Exception("Invalid subnet type. It must be 'public' or 'private'.")
 
-    # Create the number of EC2 instances specified
-    for i in range(1, number + 1):
-        ec2_name = f"{name}-{i+1:02}"  # Create unique name for each instance
-        ec2_component = EC2Component(
-            ec2_name=ec2_name,
-            ami=ami,
-            instance_type=instance_type,
-            subnet_id=subnet_id,
-            security_group_id=security_group_ids_list
-        )
+    def create_ec2_instances(security_group_ids_list, name, number, ami, instance_type, subnet_id, subnet_type, associate_public_ip):
+        # Create the number of EC2 instances specified
+        for i in range(number):
+            ec2_name = f"{name}-{i+1:02}"  # Create unique name for each instance
+            ec2_component = EC2Component(
+                ec2_name=ec2_name,
+                ami=ami,
+                instance_type=instance_type,
+                subnet_id=subnet_id,
+                security_group_id=security_group_ids_list,
+                associate_public_ip=associate_public_ip
+            )
 
-        # Export the EC2 instance details
-        pulumi.export(f"{ec2_name}_id", ec2_component.ec2_instance.id)
-        pulumi.export(f"{ec2_name}_private_ip", ec2_component.ec2_instance.private_ip)
-        pulumi.export(f"{ec2_name}_public_ip", ec2_component.ec2_instance.public_ip if subnet_type == "public" else "N/A")
-        pulumi.export(f"{ec2_name}_private_dns", ec2_component.ec2_instance.private_dns)
-        pulumi.export(f"{ec2_name}_public_dns", ec2_component.ec2_instance.public_dns if subnet_type == "public" else "N/A")
+            # Export the EC2 instance details
+            pulumi.export(f"{ec2_name}_id", ec2_component.ec2_instance.id)
+            pulumi.export(f"{ec2_name}_private_ip", ec2_component.ec2_instance.private_ip)
+            pulumi.export(f"{ec2_name}_public_ip", ec2_component.ec2_instance.public_ip if subnet_type == "public" else "N/A")
+            pulumi.export(f"{ec2_name}_private_dns", ec2_component.ec2_instance.private_dns)
+            pulumi.export(f"{ec2_name}_public_dns", ec2_component.ec2_instance.public_dns if subnet_type == "public" else "N/A")
+
+    create_ec2_instances(security_group_ids_list, name, number, ami, instance_type, subnet_id, subnet_type, associate_public_ip)
--- aws-python/compute/components/ec2_instance.py
+++ aws-python/compute/components/ec2_instance.py
@@ -1,12 +1,13 @@
-import pulumi
 from pulumi_aws import ec2
 
 class EC2Component:
-    def __init__(self, ec2_name, ami, instance_type, subnet_id, security_group_id):
+    def __init__(self, ec2_name, ami, instance_type, subnet_id, security_group_id, associate_public_ip=False):
         self.ec2_instance = ec2.Instance(
             ec2_name,
-            ami=ami,
             instance_type=instance_type,
+            ami=ami,
             subnet_id=subnet_id,
-            vpc_security_group_ids=security_group_id  # List of security group IDs
+            vpc_security_group_ids=security_group_id,
+            associate_public_ip_address=associate_public_ip,
+            tags={"Name": ec2_name}  # Add this line to set the Name tag
         )
